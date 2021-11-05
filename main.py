4523f1c00538f982e3df8c4be7613036ceabd361
actREvel <91332592+actREveL@users.noreply.github.com>
2021-11-05 17:25:27 +0100

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+from flask import Flask
+from flask import render_template
+from flask import request
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask("Your Budget Calculator")
 
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        ziel_person = request.form['vorname']
+        rueckgabe_string = ziel_person
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+        return render_template('index.html', name=rueckgabe_string)
+    return render_template('index.html')
 
+@app.route('/passt')
+def test():
+    return "Passt!"
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
