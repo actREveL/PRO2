78fa6c3af23327e06d629f0121d6a094fdf7e6d8
actREvel <91332592+actREveL@users.noreply.github.com>
2021-12-29 17:55:14 +0100

    laufende Änderungen

--- daten.py
+++ daten.py
@@ -1,23 +1,27 @@
 import json
+from datetime import datetime
 
 def speichern(name, datum, bezeichnung, kategorie, betrag, notiz):
-    datei = "uebersicht.json"
+    datei = "uebersicht3.json"
     try:
         with open(datei) as open_file:
             datei_inhalt = json.load(open_file)
     except FileNotFoundError:
-        datei_inhalt = {'name': name,
-                        'datum': datum,
-                        'bezeichnung': bezeichnung,
-                        'kategorie': kategorie,
-                        'betrag': betrag,
-                        'notiz': notiz}
+        datei_inhalt = {}
+
+    datei_inhalt[str(datetime.now())] = {'name': name,
+                'date': datum,
+                'description': bezeichnung,
+                'category': kategorie,
+                'sum': betrag,
+                'notice': notiz}
 
     with open(datei, "w") as open_file:
         json.dump(datei_inhalt, open_file, indent=4)
 
+
 def eingabe_laden():
-    datei_name = "uebersicht.json"
+    datei_name = "uebersicht3.json"
 
     try:
         with open(datei_name) as open_file:
@@ -26,3 +30,4 @@ def eingabe_laden():
         datei_inhalt = {}
 
     return datei_inhalt
+
--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import daten
+import plotly.express as px
+from plotly.offline import plot
 
 app = Flask("Your Budget Calculator")
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,15 +15,16 @@ def home():
         return render_template('formular.html')
     return render_template('index.html')
 
+# custom error page
+# invalid url
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
-@app.route("/liste")
-def auflisten():
-    eingabe = daten.eingabe_laden()
-    eingabe_liste = ""
-    for key, value in eingabe.items():
-        zeile = str(key) + ": " + value + "<br>"
-        eingabe_liste += zeile
-    return eingabe_liste
+# internal server error url
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 @app.route('/formular', methods=['GET', 'POST'])
 def formular():
@@ -36,21 +40,31 @@ def formular():
 
     return render_template('formular.html')
 
-
-# @app.route('/ausgabe')
-# def ausgabe():
-#     if form.validate_on_submit() == 'POST':
-#         if 'yes' in request.form:
-#             return render_template('uebersicht.html')
-#         elif 'no' in request.form:
-#             return render_template('formular.html')
-#     return render_template('ausgabe.html')
-
-@app.route('/uebersicht')
+@app.route('/uebersicht', methods=['GET', 'POST'])
 def uebersicht():
-    daten.eingabe_laden()
-    return render_template('uebersicht.html')
+    eingabe = daten.eingabe_laden()
 
+    return render_template('uebersicht.html', data=eingabe)
+
+@app.route('/ausgabe')
+def ausgabe_monate():
+    ausgabe = daten.eingabe_laden()
+    monate = {}
+
+    for id, values in ausgabe.items():
+        month = values["date"].split(".")[1]
+        if not monate.get(month):
+            monate[month] = values["sum"]
+        else:
+            monate[month] = monate[month] + values["sum"]
+
+    x = list(monate.keys())
+    y = list(monate.values())
+
+    fig = px.bar(x=x, y=y)
+    fig.show()
+
+    return render_template('ausgabe.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
