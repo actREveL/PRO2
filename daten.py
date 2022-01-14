eda118f926741b3bdc9ee45b047282cb92429b01
actREvel <91332592+actREveL@users.noreply.github.com>
2022-01-14 18:22:06 +0100

    auf ein Nutzer begrenzt

--- daten.py
+++ daten.py
@@ -3,16 +3,15 @@ from datetime import datetime
 
 
 # Funktion, um eingegebene Daten speichern zu können und in einem File abzuspeichern
-def speichern(name, datum, bezeichnung, kategorie, betrag, notiz):
-    datei = "ausgabe_dict.json"
+def speichern(datum, bezeichnung, kategorie, betrag, notiz):
+    datei = "ausgabe_dict1.json"
     try:
         with open(datei) as open_file:
             datei_inhalt = json.load(open_file)
     except FileNotFoundError:
         datei_inhalt = {}
 
-    datei_inhalt[str(datetime.now())] = {'Name': name,
-                                         'Datum': datum,
+    datei_inhalt[str(datetime.now())] = {'Datum': datum,
                                          'Bezeichnung': bezeichnung,
                                          'Kategorie': kategorie,
                                          'Betrag': betrag,
@@ -24,7 +23,7 @@ def speichern(name, datum, bezeichnung, kategorie, betrag, notiz):
 
 # Funktion, um gespeicherte Daten laden zu können
 def eingabe_laden():
-    datei_name = "ausgabe_dict.json"
+    datei_name = "ausgabe_dict1.json"
 
     try:
         with open(datei_name) as open_file:
--- main.py
+++ main.py
@@ -10,35 +10,37 @@ app = Flask("Your Budget Calculator")
 # Home / Startseite
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    # wird der Button "Senden" gedrückt, erscheint die Formularseite, sonst normal die Startseite
     if request.method == 'POST':
         return render_template('formular.html')
-    return render_template('index.html', user="Samira")
+    return render_template('index.html', user="Samira")  # --> hier wird der Begrüssungsname mitgegeben (Samira)
 
 # Custom Error Pages
-# Invalid URL
+# Invalid URL (Seite nicht gefunden)
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found():
     return render_template("404.html"), 404
 
 # Internal Server Error URL
 @app.errorhandler(500)
-def page_not_found(e):
+def page_not_found():
     return render_template("500.html"), 500
 
 # Formularseite --> hier können neue Ausgaben angegeben werden
 @app.route('/formular', methods=['GET', 'POST'])
 def formular():
     if request.method == 'POST':
-        name = request.form['name']
-        datum = request.form['datum']
+        datum = request.form['datum']  # Was im Feld "Name" eingegeben wurde, wird hiermit aufgerufen usw.
         bezeichnung = request.form['bezeichnung']
         kategorie = request.form['kategorie']
         betrag = request.form['betrag']
         notiz = request.form['notiz']
         # eigegebene Daten werden gespeichert
-        daten.speichern(name, datum, bezeichnung, kategorie, betrag, notiz)
+        daten.speichern(datum, bezeichnung, kategorie, betrag, notiz)
 
-    # formular.html wird gerendert und zeigt leere Zeilen an, wenn nicht eingegeben. Wird dann jedoch etwas eingegeben und gesendet, werden diese Daten mit daten.speichern (Funktion in daten.py) im json-file abgespeichert.
+    # formular.html wird gerendert und zeigt leere Zeilen an, wenn nicht eingegeben.
+    # Wird dann jedoch etwas eingegeben und gesendet, werden diese Daten mit daten.speichern (Funktion in daten.py)
+    # im json-file abgespeichert.
     return render_template('formular.html')
 
 # Übersichtsseite mit Cards --> holt Infos aus daten.py (dem Dictionary) und stellt sie "schön" dar
@@ -48,15 +50,13 @@ def uebersicht():
     filter_liste = []
     filter = ""
     filter_key = ""
+    gefiltert = False
 
+    # if-Bedingung, falls gefiltert werden möchte
     if request.method == 'POST':
-        name = request.form['name']
+        gefiltert = True
         kategorie = request.form['kategorie']
         betrag = request.form['betrag']
-
-        if name != "":
-            filter = name
-            filter_key = "Name"
 
         if kategorie != "":
             filter = kategorie
@@ -66,35 +66,34 @@ def uebersicht():
             filter = betrag
             filter_key = "Betrag"
 
-        for key, eintrag in eingabe.items():
+        for key, eintrag in eingabe.items():  # in dieser for-Schleife wird gefiltert und die leere Liste gefüllt
             if eintrag[filter_key] == filter:
                 filter_liste.append(eintrag)
 
-    return render_template('uebersicht.html', data=eingabe, user=filter_liste)
+    return render_template('uebersicht.html', data=eingabe, user=filter_liste, Kat=gefiltert)
 
 # Übersichtsseite mit Balkendiagramm
 @app.route('/ausgabe', methods=['GET', 'POST'])
 def statistik():
-    ausgabe = daten.eingabe_laden() # Dictionary (alle Daten) von daten.py wird geladen
-    monate = {} # leeres dict für Monate 01-12 (Jan-Dez) definiert, welches mit der unteren for-Schleife gefüllt wird
-
+    ausgabe = daten.eingabe_laden()  # Dictionary (alle Daten) von daten.py wird geladen
+    monate = {}  # leeres dict für Monate 01-12 (Jan-Dez) definiert, welches mit der unteren for-Schleife gefüllt wird
 
     for id, values in ausgabe.items():
-        month = values["Datum"].split(".")[1] # Datum splitten, sodass nur Monat (01 = Jan etc.) übrigbleibt
+        month = values["Datum"].split(".")[1]  # Datum splitten, sodass nur Monat (01 = Jan etc.) übrigbleibt
 
         if not monate.get(month):
-            monate[month] = float(values["Betrag"]) # Betrag hinzufügen
+            monate[month] = float(values["Betrag"])  # Betrag hinzufügen
         else:
-            monate[month] = monate[month] + float(values["Betrag"]) # Betrag hinzufügen und zu Monat addieren
+            monate[month] = monate[month] + float(values["Betrag"])  # Betrag hinzufügen und zu Monat addieren
 
-    x = list(monate.keys()) # Liste für Monate --> kann so in Barchart eingelesen werden
-    y = list(monate.values()) # Liste für Beträge/Monat --> kann so in Barchart eingelesen werden
-    # nutzer = ausgabe["Name"] --> wieso funktioniert das nicht?!
+    x = list(monate.keys())  # Liste für Monate --> kann so in Barchart eingelesen werden
+    y = list(monate.values())  # Liste für Beträge/Monat --> kann so in Barchart eingelesen werden
+    # nutzer = ausgabe.get("Name") #--> wieso funktioniert das nicht?!
 
     # hier wird der Barchart erstellt bzw. werden die Daten mitgegeben, welche für den Barchart erforderlich sind
     fig = px.bar(x=x, y=y, labels={"x": "Monate",
                                    "y": "Betrag in CHF"},
-                 # color=nutzer,
+                 # color=y[1],
                  title="Ausgaben über die Monate hinweg")
     # https://plotly.com/python/figure-labels/ für Benennung der Achsen & Titel mittels labels
     # https://careerkarma.com/blog/python-typeerror-unhashable-type-list/ --> für Problembehebung "unhashable list"
@@ -108,14 +107,23 @@ def statistik():
     for element in sum_liste:
         sum += float(element)
 
+    # Summe Monat berechnen
+    sum_monat = 0
+    if request.method == 'POST':
+        monat = request.form['monat']
+        if monat in x:
+            for element in y:
+                sum_monat = element
+
     # Budget berechnen
     verfuegbar = 0
     if request.method == 'POST':
         budget = request.form['budget']
-        budget = float(budget) - float(sum)
+        budget = float(budget) - float(sum_monat)
         verfuegbar = verfuegbar + budget
 
-    # rendern des Templates "ausgabe.html" zeigt dann den Barchart an und nimmt Monate und Total mit für die Liste mit den Beiträgen/Monat und die Gesamtausgaben über alle Einträge hinweg
+    # rendern des Templates "ausgabe.html" zeigt dann den Barchart an und nimmt Monate und Total mit für die Liste
+    # mit den Beiträgen/Monat und die Gesamtausgaben über alle Einträge hinweg
     return render_template('ausgabe.html', viz_div=div, monate=monate, total=sum, ergebnis=verfuegbar)
 
 if __name__ == "__main__":
